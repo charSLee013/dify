0d672842cca1718269053e693db82ff8b550d6d3
charSLee <735365895@qq.com>
2023-11-22 10:39:46 +0800

    chore:CustomChatLLM test cases

--- api/core/third_party/langchain/llms/custom_llm_test.py
+++ api/core/third_party/langchain/llms/custom_llm_test.py
@@ -9,28 +9,24 @@ class TestCustomChatLLM(unittest.TestCase):
 
     def setUp(self):
         # Set up any necessary objects for testing
-        self.custom_chat = CustomChatLLM(base_url="http://43.156.175.194:8000")
+        self.server_url = "http://43.156.130.37:8000"
 
     def test_generate(self):
-        # Mock the post method of CustomClient to simulate API response
-        mock_response = {"choices": [{"role": "assistant", "content": "Generated text"}]}
-
+        client = CustomChatLLM(server_url=self.server_url)
         # Call the generate method with some test messages
         messages = [HumanMessage(content="Hello")]
-        result = self.custom_chat._generate(messages)
+        result = client.generate([messages])
 
         # Assert that the result is of the expected type
-        self.assertIsInstance(result, ChatResult)
+        self.assertTrue(len(result.generations)>0)
 
         # Add more assertions based on the expected behavior of the generate method
 
     def test_stream(self):
+        client = CustomChatLLM(server_url=self.server_url,streaming=True)
         # Call the stream method with some test messages
         messages = [HumanMessage(content="Hello")]
-        chunks = []
-        for chunk in self.custom_chat._stream(messages=messages):
-            chunks.append(chunk)
-        # chunks = list(self.custom_chat._stream(messages))
+        chunks = list(client._stream(messages))
 
         self.assertTrue(len(chunks)>0)
         self.assertTrue(all(isinstance(chunk, ChatGenerationChunk) for chunk in chunks))
